92aed1c6c41b3fccd398fbd311363f58bcf90830
Christopher Pedersen <chris@topherpedersen.com>
2022-02-15 11:12:06 -0600

    install react-native-reanimated (v2)

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  /*
+   * Reanimated plugin has to be listed last
+   * https://docs.swmansion.com/react-native-reanimated/docs/fundamentals/installation
+   */
+  plugins: [
+    'react-native-reanimated/plugin',
+  ],
 };
